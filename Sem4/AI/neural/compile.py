0b3c8af87e00b40ec45d91c0c03292cdd0340299
nskowron <nel.skowronek@gmail.com>
2025-03-22 14:10:01 +0100

    removed last layer and strides

--- Sem4/AI/neural/compile.py
+++ Sem4/AI/neural/compile.py
@@ -9,7 +9,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
 
     # downsamples the image (bochen)
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+    tf.keras.layers.MaxPooling2D((2, 2)),
     
     # relu - rectified linear unit
     # zero negative values, shut down neuron, help with vanishing gradient
@@ -19,7 +19,6 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(),
 
     # final interpretation layer
-    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
