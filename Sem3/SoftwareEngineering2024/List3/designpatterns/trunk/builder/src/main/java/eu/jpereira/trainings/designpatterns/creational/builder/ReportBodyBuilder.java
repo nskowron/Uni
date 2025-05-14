ff9631ed6417b7efa83eb45f6a0a47474a8db11d
nskowron <76043223+nskowron@users.noreply.github.com>
2025-05-14 18:47:50 +0200

    Delete Sem3/SoftwareEngineering2024 directory
    WHY WAS IT EVEN HERE

--- Sem3/SoftwareEngineering2024/List3/designpatterns/trunk/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportBodyBuilder.java
+++
@@ -1,9 +0,0 @@
-package eu.jpereira.trainings.designpatterns.creational.builder;
-
-import eu.jpereira.trainings.designpatterns.creational.builder.model.ReportBody;
-import eu.jpereira.trainings.designpatterns.creational.builder.model.SaleEntry;
-
-public interface ReportBodyBuilder
-{
-    public ReportBody buildReportBody(SaleEntry saleEntry);
-}
