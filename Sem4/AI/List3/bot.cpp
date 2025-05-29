92809cabeed08f114f5c2a9dd50a463099f4fdc1
nskowron <nel.skowronek@gmail.com>
2025-05-29 18:42:19 +0200

    frame for further work

---
+++ Sem4/AI/List3/bot.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+#include <limits>
+
+short board[5][5] = {
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0}
+};
+
+int get_valid_moves(short moves[]) {
+    int c = 0;
+    for(int i = 0; i < 5; ++i) {
+        for(int j = 0; j < 5; ++j) {
+            if(board[i][j] == 0) {
+                moves[c++] = i * 10 + j;
+            }
+        }
+    }
+    return c;
+}
+
+short best_move(bool maximizing_player) {
+    short moves[25];
+    int c = get_valid_moves(moves);
+    
+    return moves[0]; // Placeholder: return the first valid move
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " <IP> <port> <nr> <nick>" << std::endl;
+        return -1;
+    }
+
+    char server_message[16] = {0};
+    std::string player_message;
+
+    // Open socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0) {
+        std::cerr << "Error creating socket" << std::endl;
+        return -1;
+    }
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    // Connect to server
+    if(connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Error connecting to server" << std::endl;
+        return -1;
+    }
+    std::cout << "Connected to server at " << argv[1] << ":" << argv[2] << std::endl;
+
+    // Receive server message
+    if(recv(sock, server_message, sizeof(server_message), 0) < 0) {
+        std::cerr << "Error receiving server message" << std::endl;
+        return -1;
+    }
+
+    // Send player number and nickname
+    player_message = std::string(argv[3]) + " " + argv[4] + "\n";
+    if(send(sock, player_message.c_str(), player_message.size(), 0) < 0) {
+        std::cerr << "Error sending player info" << std::endl;
+        return -1;
+    }
+    std::cout << "Sent player number and nickname: " << player_message;
+
+    // Play
+    short player_number = atoi(argv[3]);
+    bool maximizing_player = (player_number == 1);
+    bool game_over = false;
+    short msg, move;
+    while(!game_over) {
+        // Receive move from server
+        memset(server_message, 0, sizeof(server_message));
+        if(recv(sock, server_message, sizeof(server_message), 0) < 0) {
+            std::cerr << "Error receiving move message" << std::endl;
+            return -1;
+        }
+        msg = atoi(server_message);
+        move = msg % 100 - 11;
+        msg /= 100;
+        if(move != 0) {
+            board[move / 10][move % 10] = 3 - player_number;
+        }
+        if((msg == 0) || (msg == 6)) {
+            // Calculate minimax
+            move = best_move(maximizing_player);
+            board[move / 10][move % 10] = player_number;
+
+            // Send move back to server
+            player_message = std::to_string(move + 11);
+            if(send(sock, player_message.c_str(), player_message.size(), 0) < 0) {
+                std::cerr << "Error sending move" << std::endl;
+                return -1;
+            }
+        } else {
+            // Game over
+            game_over = true;
+            if(msg == 1) {
+                std::cout << "You win!" << std::endl;
+            } else if(msg == 2) {
+                std::cout << "You lose!" << std::endl;
+            } else if(msg == 3) {
+                std::cout << "It's a draw!" << std::endl;
+            }
+        }
+    }
+}
