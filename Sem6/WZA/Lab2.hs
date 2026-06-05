bd9f0ee3fdb21822c955dda7e365816e2c0c2d08
nskowron <nel.skowronek@gmail.com>
2026-06-06 00:49:05 +0200

    algebra exc2 without this supid quadrofolium

--- Sem6/WZA/Lab2.hs
+++ Sem6/WZA/Lab2.hs
@@ -23,8 +23,22 @@ instance Ord a => Ord (Lex a) where
 
 -- lex multiindex based on a permutation (0..n-1)
 -- from greatest to least
-lexPerm :: [Int] -> Multiindex a -> Lex a
-lexPerm perm (Multiindex xs) = Lex $ Multiindex (map (xs !!) perm)
+miPerm :: [Int] -> Multiindex a -> Multiindex a
+miPerm perm (Multiindex xs) = Multiindex (map (xs !!) perm)
+
+fromMiPerm :: (Num a) => [Int] -> Multiindex a -> Multiindex a
+fromMiPerm perm (Multiindex xs) = 
+    Multiindex $ V.toList $ V.replicate (length xs) 0 V.// zip perm xs
+
+-- helpers, temporary
+mapExponents :: (Ord b) => (a -> b) -> Polynomial a k -> Polynomial b k
+mapExponents f (Polynomial xs) = Polynomial (Map.mapKeys f xs)
+
+miPermLex :: [Int] -> Lex a -> Lex a
+miPermLex perm (Lex mi) = Lex (miPerm perm mi)
+
+fromMiPermLex :: (Num a) => [Int] -> Lex a -> Lex a
+fromMiPermLex perm (Lex mi) = Lex (fromMiPerm perm mi)
 
 -- Graded Lex
 newtype GradedLex a = GradedLex (Multiindex a)
@@ -40,22 +54,18 @@ instance (Ord a, Num a) => Ord (GradedLex a) where
             then compare xs ys
             else compare diff 0
 
-gradedLexPerm :: [Int] -> Multiindex a -> GradedLex a
-gradedLexPerm perm (Multiindex xs) = GradedLex $ Multiindex (map (xs !!) perm)
-
 -- Polynomial Reduce
 polynomialReduce :: (Ord a, Eq k, Num a, Num k, Fractional k) => 
     Polynomial a k -> [Polynomial a k] -> ([Polynomial a k], Polynomial a k)
 polynomialReduce f gs = go f (V.replicate (length gs) 0) 0
     where
         vgs = V.fromList gs
-        vltgs = V.map ltP vgs -- vector of leading terms of gs
+        vltgs = V.map (\g -> Polynomial $ Map.fromList [ltP g]) vgs -- vector of leading terms of gs
         go 0 qs r = (V.toList qs, r)
         go p qs r = 
             let ltp = Polynomial (Map.fromList [ltP p])
-            in case V.findIndex (\g -> snd (divP ltp (Polynomial (Map.fromList [ltP g]))) == 0) vgs of
+            in case V.findIndex (\ltg -> snd (divP ltp ltg) == 0) vltgs of
                 Nothing -> go (p - ltp) qs (r + ltp)
                 Just i ->
-                    let ltgi = Polynomial (Map.fromList [vltgs V.! i])
-                        q = fst $ divP ltp ltgi
+                    let q = fst $ divP ltp (vltgs V.! i)
                     in go (p - q * (vgs V.! i)) (qs V.// [(i, (qs V.! i) + q)]) r
